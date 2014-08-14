856551442fa805330669ea3b651253c3caecddfb
administrator <administrator@administrator-virtual-machine.(none)>
2014-08-14 10:11:46 -0400

    time saved as long int

--- web_service/network_monitor/models.py
+++ web_service/network_monitor/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class History(models.Model):
-    date   = models.DateTimeField()
+    date   = models.BigIntegerField()
     client = models.CharField(max_length=20)
     host   = models.CharField(max_length=50)
     value  = models.IntegerField(default=0)
