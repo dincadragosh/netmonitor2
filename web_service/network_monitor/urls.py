88f2476ed2ac402c84199f3e9c119a9e9e5c6600
administrator <administrator@administrator-virtual-machine.(none)>
2014-08-15 19:34:46 -0400

    0 barat

--- web_service/network_monitor/urls.py
+++ web_service/network_monitor/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from network_monitor import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.history_table, name='history_table'),
+    url(r'^$', views.history_graph_users_time, name='history_graph_users_time'),
+    url(r'^history_table', views.history_table, name='history_table'),
     url(r'^history_graph_user', views.history_graph_user, name='history_graph_user'),
-    url(r'^history_users', views.history_graph_users_time, name='history_graph_users_time')
 )
