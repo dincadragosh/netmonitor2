e8de1f192c07fbc84ca336b48dcca49ee39e6d11
administrator <administrator@administrator-virtual-machine.(none)>
2014-08-16 19:34:13 -0400

    dev on web-service

--- web_service/network_monitor/urls.py
+++ web_service/network_monitor/urls.py
@@ -4,5 +4,5 @@ from network_monitor import views
 urlpatterns = patterns('',
     url(r'^$', views.history_graph_users_time, name='history_graph_users_time'),
     url(r'^history_table', views.history_table, name='history_table'),
-    url(r'^history_graph_user', views.history_graph_user, name='history_graph_user'),
+    url(r'^history_graph_user1', views.history_graph_user_host_time, name='history_graph_user_host_time'),
 )
--- web_service/network_monitor/views.py
+++ web_service/network_monitor/views.py
@@ -3,9 +3,10 @@ import calendar
 import time
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from network_monitor.models import History
+
 
 
 def history_table(request):
@@ -20,16 +21,22 @@ def history_table(request):
     context = {'history_list': history_list}
     return render(request, 'network_monitor/history_table.html', context)
 
-def history_graph_user(request):
+
+
+def history_graph_user_host_time(request, user_mac):
+    
     start = request.POST.get('date_start')
     end   = request.POST.get('date_end')
     if start and end:
         start = int(datetime.strptime(start, '%Y-%m-%dT%H:%M').strftime("%s"))
         end = int(datetime.strptime(end, '%Y-%m-%dT%I:%M').strftime("%s"))
-        history_list = History.objects.order_by('date').filter(date__gte=start).filter(date__lte=end)
+        history_list = History.objects.filter(client__exact=user_mac).filter(date__gte=start).filter(date__lte=end).order_by('host', 'date')
     else:
-        history_list = History.objects.order_by('host','date')
+        history_list = History.objects.filter(client__exact=user_mac).order_by('host', 'date')
     
+    if (!history_list):
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
     user_graph_list = []
     cur_host = None
     for element in history_list:
@@ -43,8 +50,9 @@ def history_graph_user(request):
     context = {'user_graph_list': user_graph_list}
     return render(request, 'network_monitor/history_graph_user.html', context)
 
+
+
 def history_graph_users_time(request):
-    print('>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<')
     start = request.POST.get('date_start')
     end   = request.POST.get('date_end')
     if start and end:
@@ -55,16 +63,17 @@ def history_graph_users_time(request):
         history_list = History.objects.order_by('client','date')
 
     users_time_list = []
+    clients_list = []
     cur_client = None
     for element in history_list:
         if element.client != cur_client:
             cur_client = element.client
+            clients_list.append(element.client)
             users_time_list.append({'client': element.client, 'value': [{'first': element.date, 'second': element.value}]})
         elif element.date == users_time_list[-1]['value'][-1]['first']:
             users_time_list[-1]['value'][-1]['second'] += element.value 
         else:
             users_time_list[-1]['value'].append({'first': element.date, 'second': element.value})
-    print('intra')
-
-    context = {'users_time_list': users_time_list}
+    
+    context = {'users_time_list': users_time_list, 'clients_list': clients_list}
     return render(request, 'network_monitor/history_graph_users_time.html', context)
